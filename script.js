e1fbdca7b942538321757820b2801cb82f446623
fuadarif127 <fuad.arif127@gmail.com>
2025-08-28 02:39:56 +0600

    responsive done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hi")
+const mainHeart = document.getElementsByClassName("main-heart");
+console.log(mainHeart);
+for (const heart of mainHeart) {
+  heart.addEventListener("click", function () {
+    const heartOfMain = document.getElementById("nav-heart").innerText;
+    let NavHeart = Number(heartOfMain) + 1;
+    document.getElementById("nav-heart").innerText = NavHeart;
+  });
+}
